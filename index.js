45dac9e6aedb55894bb747e2c40edf66c1d28804
insideoutzombie <david@web3devs.com>
2019-08-13 11:45:10 -0500

    general push

---
+++ index.js
@@ -0,0 +1,25 @@
+//
+// #### Exercise 1
+// 1. Create an array.
+// 2. Add four names to the array.
+// 3. Print the array
+let fourNames = [];
+fourNames.push("Ghost", "mark", "Cobra", "Civic");
+console.log(fourNames);
+
+// #### Exercise 2
+// 1. Create an array with 5 numbers in it.
+// 2. Print the numbers.
+let numbersArray = [1,3,5,7,9];
+console.log(numbersArray);
+
+
+// #### Exercise 3
+// 1. Create an array with 4 names.
+// 2. Print the third item in the array.
+// 3. Delete the second item.
+// 4. Print the third item in the array again.
+let userNames = ["Max", "Dan", "john", "jack"];
+console.log(userNames[2]);
+userNames.splice([2]);
+console.log(userNames[2]);
